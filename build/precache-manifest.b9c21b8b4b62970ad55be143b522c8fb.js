2fe96661128a6f2564e85849d7f067a69dc0bcd6
FernandoLimas <nando_filipe@hotmail.com>
2023-01-22 19:22:40 -0300

    first commit

---
+++ build/precache-manifest.b9c21b8b4b62970ad55be143b522c8fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b297e44170e60414bebd41dd60d68d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e9ce34272a0fe849c0af",
+    "url": "./static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "b00dda8e2277ee94f5f5",
+    "url": "./static/js/2.eb2e9f0a.chunk.js"
+  },
+  {
+    "revision": "e9ce34272a0fe849c0af",
+    "url": "./static/js/main.120baea6.chunk.js"
+  },
+  {
+    "revision": "dbdf31d36a9c958b77a8",
+    "url": "./static/js/runtime-main.e38fd29e.js"
+  }
+]);
